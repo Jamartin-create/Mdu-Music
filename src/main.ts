f584110d8e1f885bad9354a23170192b8b3d64b6
LilMartin <martin_du@qq.com>
2022-10-19 21:04:30 +0800

    init: 引入pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import router from './router'
+import { createPinia } from 'pinia'
+import piniaPluginPresistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 
 const app = createApp(App)
-
+const pinia = createPinia()
+pinia.use(piniaPluginPresistedstate)
+app.use(pinia)
 app.use(router)
 app.mount('#app')
